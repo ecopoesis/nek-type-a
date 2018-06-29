65509361ffd0020ad29d26dc4285f0802590a036
Mike Roberts <miker@miker.org>
2018-06-28 21:18:25 -0400

    right top plate

--- body/body.py
+++ body/body.py
@@ -7,10 +7,11 @@ import quaternion as quat
 from svgpathtools import svg2paths2, Line
 import logging as log
 import sys
+import itertools
 # import pydevd
 log.basicConfig(stream=sys.stderr, level=log.DEBUG)
 
-thickness = 10.0
+top_plate_depth = 9.0
 
 # angles
 tent = 17.5
@@ -48,6 +49,8 @@ right_x = right_plate_x + (2 * extra_base)
 y = plate_y + (2 * extra_base)
 extrude = 150
 
+SVG_PATH = '/opt/cadquery/build_data/'
+
 depth_path = cq.Workplane("XZ").lineTo(0, extrude)
 
 # global 0,0,0 is the pivot point where the halves meet on the bottom
@@ -59,17 +62,20 @@ def right():
     small_corner_x = right_x-small_corner+(small_corner*math.sin(math.radians(45)))
     small_corner_y = -small_corner+(small_corner*math.cos(math.radians(45)))
 
-    return cq.Workplane("XY") \
+    wp = cq.Workplane("XY") \
         .transformed(rotate=cq.Vector(0, tent, split/2)) \
-        .transformed(rotate=cq.Vector(-slope, 0, 0)) \
-        .lineTo(0, -(y+wrist)) \
+        .transformed(rotate=cq.Vector(-slope, 0, 0))
+
+    return wp.lineTo(0, -(y+wrist)) \
         .lineTo(right_x-big_corner, -(y+wrist)) \
         .threePointArc((big_corner_x, big_corner_y), (right_x, -y-wrist+big_corner)) \
         .lineTo(right_x, -small_corner) \
         .threePointArc((small_corner_x, small_corner_y), (right_x-small_corner, 0)) \
         .close() \
-        .sweep(depth_path)
+        .sweep(depth_path) \
+        .cut(svg('right_top', right_workplane().center(extra_base, -extra_base), -top_plate_depth, [3, 4, 5, 6, 7, 8]))
 
+#[3, 4, 5, 6, 7, 8]
 
 def left():
     big_corner_x = -left_x+big_corner-(big_corner*math.sin(math.radians(45)))
@@ -131,6 +137,30 @@ def left_back_corner():
         .plane.toWorldCoords((-left_x+small_corner, 0))
 
 
+def build_plane(p1, p2, p3, origin, z_rot=0):
+    # These two vectors are in the plane
+    v1 = p3 - p1
+    v2 = p2 - p1
+
+    # the cross product is a vector normal to the plane
+    normal = np.cross(v1, v2)
+
+    return cq.Plane(origin, (math.cos(math.radians(z_rot)), math.sin(math.radians(z_rot)), 0), (normal[0], normal[1], normal[2]))
+
+
+def right_plane():
+    right_corner = right_back_corner()
+    right_gap = right_gap_bottom()
+    p1 = np.array([0, 0, extrude])
+    p2 = np.array([right_corner.x, right_corner.y, right_corner.z+extrude])
+    p3 = np.array([right_gap.x, right_gap.y, right_gap.z+extrude])
+    return build_plane(p1, p2, p3, (0, 0, extrude), split/2)
+
+
+def right_workplane():
+    return cq.Workplane(right_plane())
+
+
 def back():
     right_corner = right_back_corner()
     left_corner = left_back_corner()
@@ -139,15 +169,7 @@ def back():
     p1 = np.array([0, 0, extrude])
     p2 = np.array([right_corner.x, right_corner.y, right_corner.z+extrude])
     p3 = np.array([left_corner.x, left_corner.y, left_corner.z+extrude])
-
-    # These two vectors are in the plane
-    v1 = p3 - p1
-    v2 = p2 - p1
-
-    # the cross product is a vector normal to the plane
-    normal = np.cross(v1, v2)
-
-    plane = cq.Plane((0, 0, extrude), (1, 0, 0), (normal[0], normal[1], normal[2]))
+    plane = build_plane(p1, p2, p3, (0, 0, extrude))
 
     right_transformed = plane.toLocalCoords(cq.Vector(right_corner.x, right_corner.y, right_corner.z+extrude))
     left_transformed = plane.toLocalCoords(cq.Vector(left_corner.x, left_corner.y, left_corner.z+extrude))
@@ -161,7 +183,6 @@ def back():
 
 def chop():
     depth = right_back_corner().z * -2
-    log.debug(right_back_corner())
     return cq.Workplane("XY") \
         .transformed(offset=(0, right_gap_bottom().y / 2, right_gap_bottom().z - depth + (depth / 2) + fillet_r)) \
         .box(800, 400, depth)
@@ -172,21 +193,43 @@ def debox(x, y, z):
         .transformed(offset=(x, y, z)) \
         .box(50, 50, 50)
 
-def svg(svg_file, workplane):
-    paths, attributes, svg_attributes = svg2paths2(f'/opt/cadquery/build_data/{svg_file}.svg')
-    polys = list(map(lambda path: (list(map(lambda segment: (segment.start.real, segment.start.imag), path))), paths))
-    log.debug(polys[0][0])
-    log.debug(polys[0][1:])
-    return workplane.moveTo(*polys[1][0]).polyline(polys[1][1:]).close().extrude(extrude)
+def svg(svg_file, workplane, extrude, shapes=None):
+    """
+    extrude shapes in the svg file on the workplane
+    :param svg_file: file name of svg file
+    :param workplane: workplane to add svg to
+    :param extrude: amount to extrude by
+    :param shapes: list of shapes in the svg file to extrude (null extrudes all)
+    :return: workplane
+    """
+    paths, attributes, svg_attributes = svg2paths2(f'{SVG_PATH}{svg_file}.svg')
+    # invert y-coordinate because svg measures from top-left
+    polys = list(map(lambda path: (list(map(lambda segment: (segment.start.real, -segment.start.imag), path))), paths))
+    min_x = reduce(lambda acc, i: min(acc, i[0]), itertools.chain.from_iterable(polys), sys.maxsize)
+    max_y = reduce(lambda acc, i: max(acc, i[1]), itertools.chain.from_iterable(polys), -sys.maxsize)
+
+    for idx, poly in enumerate(polys):
+        if shapes is None or idx in shapes:
+            zeroed = map(lambda point: (point[0] - min_x, point[1] - max_y), poly)
+            workplane = workplane.moveTo(*zeroed[0]).polyline(zeroed[1:]).close().extrude(extrude)
+    return workplane
 
 body = center() \
     .union(right()) \
     .union(left()) \
-    .union(back()) \
     .cut(chop())
 
 #     .edges().fillet(fillet_r) \
 
-wp = cq.Workplane("XY")
+show_object(body)
 
-show_object(svg('right_top', wp))
+## right_top
+# 0 - outside
+# 1, 2 - screw
+# 3 - main
+# 4 - arrows
+# 5 - navigation
+# 6 - low function
+# 7 - high function
+# 8 - audio
+# 9, 10 - screw
