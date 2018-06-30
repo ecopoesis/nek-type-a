8e2e14238a8004474c4e8fce6e59ab810eae34b8
Mike Roberts <miker@miker.org>
2018-06-29 21:12:31 -0400

    hollow bottoms

--- body/body.py
+++ body/body.py
@@ -38,14 +38,6 @@ fillet_r = 10
 small_corner = 10
 big_corner = 90
 
-# quaternions
-#right_quat = quat.from_euler_angles(alpha, beta, gamma)
-#left_quat = Q_Mul(Q_Mul(Quat([0,-1,0],tent), Quat([0,0,-1],split/2)), Quat([-1,0,0],slope))
-#right_quat = Q_Mul(Q_Mul(Quat([0,1,0],tent), Quat([0,0,1],split/2)), Quat([-1,0,0],slope))
-
-#function _Quat(a,s,w) = [a[0]*s, a[1]*s, a[2]*s, w];
-#function Quat(ax, ang) = _Quat(ax/norm(ax), sin(ang/2), cos(ang/2));
-
 left_x = left_plate_x + (2 * extra_base)
 right_x = right_plate_x + (2 * extra_base)
 y = plate_y + (2 * extra_base)
@@ -56,7 +48,6 @@ SVG_PATH = '/opt/cadquery/build_data/'
 depth_path = cq.Workplane("XZ").lineTo(0, extrude)
 
 # global 0,0,0 is the pivot point where the halves meet on the bottom
-
 
 def right():
     big_corner_x = right_x-big_corner+(big_corner*math.sin(math.radians(45)))
@@ -75,7 +66,9 @@ def right():
         .threePointArc((small_corner_x, small_corner_y), (right_x-small_corner, 0)) \
         .close() \
         .sweep(depth_path) \
-        .cut(svg('right_top', right_workplane().center(extra_base, -extra_base), -top_plate_depth, [3, 4, 5, 6, 7, 8]))
+        .cut(svg('right_top', right_workplane().center(extra_base, -extra_base), -top_plate_depth, [3, 4, 5, 6, 7, 8])) \
+        .cut(svg('right_top', right_workplane().transformed(offset=(0,0,-top_plate_depth)).center(extra_base, -extra_base), -extrude, [0]))
+
 
 def left():
     big_corner_x = -left_x+big_corner-(big_corner*math.sin(math.radians(45)))
@@ -93,7 +86,8 @@ def left():
         .threePointArc((small_corner_x, small_corner_y), (-left_x+small_corner, 0)) \
         .close() \
         .sweep(depth_path) \
-        .cut(svg('left_top', left_workplane().center(-left_plate_x-extra_base, y-extra_base), top_plate_depth, [3, 4, 5], False))
+        .cut(svg('left_top', left_workplane().center(-left_plate_x-extra_base, y-extra_base), top_plate_depth, [3, 4, 5], False)) \
+        .cut(svg('left_top', left_workplane().transformed(offset=(0,0,top_plate_depth)).center(-left_plate_x-extra_base, y-extra_base), extrude, [0]))
 
 
 def center():
